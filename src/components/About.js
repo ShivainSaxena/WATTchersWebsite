957cac6524370db34f1868766ff6b4000fb3ff37
Shivain Saxena <shivainsaxena@gmail.com>
2024-02-23 09:16:29 -0700

    picture change

--- src/components/About.js
+++ src/components/About.js
@@ -18,12 +18,12 @@ const About = () => {
                 <p className="profileDesc">Role: <br />- Fullstack Software Engineer <br />- Embedded Software Testing <br />Linkedln: <a href="http://www.linkedin.com/in/aniketh-kini-605566217" target='_blank' rel='noreferrer'>in/aniketh-kini/</a> <br />Email: <a href="mailto:anikeths@ualberta.ca">anikeths@ualberta.ca</a><br />Mobile: +1 (780) 600-0388</p>
             </div>
             <div className="cardContainer">
-                <img src={AE} alt="profile" className='profilePic'/>
+                <img src={AT} alt="profile" className='profilePic'/>
                 <h1 className='profileName'>Alexander Torres Jr.</h1>
                 <p className="profileDesc">Role: <br />- Circuit Designer and Soldering Technician <br />- Embedded Systems Engineer <br />Linkedln: <a href= "https://www.linkedin.com/in/alexander-jr-torres/" target='_blank' rel='noreferrer'>in/alexander-jr-torres/</a> <br />Email: <a href="mailto:astorre1@ualberta.ca">astorre1@ualberta.ca </a><br />Mobile: +1 (403) 465-0066</p>
             </div>
             <div className="cardContainer">
-                <img src={AT} alt="profile" className='profilePic'/>
+                <img src={AE} alt="profile" className='profilePic'/>
                 <h1 className='profileName'>Alex Ermube</h1>
                 <p className="profileDesc">Role: <br />- Structural Engineer <br />- Graphic Designer <br />Linkedln: <a href= "https://www.linkedin.com/in/alex-ermube/" target='_blank' rel='noreferrer'>in/alex-ermube/</a> <br />Email: <a href="mailto:aermube@ualberta.ca">aermube@ualberta.ca </a><br />Mobile: +1 (587) 501-2539</p>
             </div>
